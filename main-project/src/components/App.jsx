f94b4c17239ace3267ac70ce8f264b435e757e2b
larakoseff <larakoseff@gmail.com>
2023-11-13 20:36:59 +0200

    Add animation via React Spring

--- main-project/src/components/App.jsx
+++ main-project/src/components/App.jsx
@@ -1,66 +1,81 @@
-import { Button, TextField, Alert, Paper, IconButton } from "@mui/material"
-import { Delete } from '@mui/icons-material'
-import { useTasks } from './App.useTasks'
-import styled from '@emotion/styled'
+import { Button, TextField, Alert, Paper, IconButton } from "@mui/material";
+import { Delete } from "@mui/icons-material";
+import { useTransition, animated } from "@react-spring/web";
+
+import { useTasks } from "./App.useTasks";
+import styled from "@emotion/styled";
 
 const StyledTextField = styled(TextField)`
-margin: 1rem;
-background: #eee;
+  margin: 1rem;
+  background: #eee;
+  font-family: "Karla", sans-serif;
 `;
 
 const StyledButton = styled(Button)`
-margin: 1rem;
-background: #26FFE6;
-color: black;
-background-hover
+  margin: 1rem;
+  background: #26ffe6;
+  color: black;
+  font-family: "Karla", sans-serif;
 `;
 
 const StyledPaper = styled(Paper)`
-list-style: none;
-margin: 1rem;
-background: #eee;
-text-indent: 10px;
+  list-style: none;
+  margin: 1rem;
+  background: #eee;
+  text-indent: 10px;
+  font-family: "Karla", sans-serif;
 `;
 
-const List = styled.ul `
-padding: 0;
-list-style: none;
-`
+const List = styled.ul`
+  padding: 0;
+  list-style: none;
+`;
 
 export const App = () => {
-const { tasks, remove, add, message } = useTasks()
+  const { tasks, remove, add, message } = useTasks();
 
-const formSubmit = (event) => {
+  const list = useTransition(tasks, {
+    from: { x: -200, opacity: 0 },
+    enter: { x: 0, opacity: 1 },
+    leave: { x: 200, opacity: 0 },
+  });
+
+  const formSubmit = (event) => {
     event.preventDefault();
     const { target } = event;
     const data = new FormData(target);
     const { title } = Object.fromEntries(data);
-    add(title)
+    add(title);
     target.reset();
   };
 
   return (
-    <div className="red-button">
-      <form onSubmit={formSubmit}>
-        <StyledTextField label="New Task" name="title" variant="filled"/>
-        <StyledButton type="submit" size="large" variant="contained">SAVE</StyledButton>
-      </form>
+    <>
+      <div className="red-button">
+        <form onSubmit={formSubmit}>
+          <StyledTextField label="New Task" name="title" variant="filled" />
+          <StyledButton type="submit" size="large" variant="contained">
+            SAVE
+          </StyledButton>
+        </form>
 
-     {message && <Alert severity="warning">{message}</Alert>}
+        {message && <Alert severity="warning">{message}</Alert>}
 
-
-      <List>
-        {tasks.map(({ id, title }) => {
-          return (
-           <StyledPaper key={id} component="li">
-            <span>{title}</span>
-            <IconButton size="small" onClick={() => remove(id)}>
-              <Delete/>
-            </IconButton>
-            </StyledPaper>
-          )
-        })}
-      </List>
-    </div>
+        <List>
+          {list((style, { id, title } ) => {
+            return (
+              <animated.div key={IDBDatabase} style={style}>
+                <StyledPaper component="li">
+                  <span>{title}</span>
+                  <IconButton size="small" onClick={() => remove(id)}>
+                    <Delete />
+                  </IconButton>
+                </StyledPaper>
+              </animated.div>
+            );
+          })}
+        </List>
+      </div>
+    </>
   );
 };
