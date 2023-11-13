a3ce805224ee1d0779100633d5156353cdc4ed5e
larakoseff <larakoseff@gmail.com>
2023-11-13 13:24:12 +0200

    Update boxes

--- main-project/src/App.jsx
+++ main-project/src/App.jsx
@@ -1,23 +1,18 @@
 import React from "react"
 import boxes from "./boxes.jsx"
+import Box from "./Box.jsx"
 
-export default function App() {
-    const [squares, setSquares] = React.useState(boxes)
+export default function App(props) {
     
-    const squareElements = squares.map(square => (
-        <div className="box" key={square.id}></div>
-    ))
-    /**
-     * Challenge part 1:
-     * 1. Initialize state with the default value of the
-     *    array pulled in from boxes.js
-     * 2. Map over that state array and display each one
-     *    as an empty square (black border, transparent bg color)
-     *    (Don't worry about using the "on" property yet)
-     */
+        const [squares, setSquares] = React.useState(boxes)
+        
+        const squareElements = squares.map(square => (
+            <Box key={square.id} on={square.on} />
+        ))
     return (
         <main>
             {squareElements}
         </main>
     )
 }
+
